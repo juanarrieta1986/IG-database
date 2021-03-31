ea099124bf16b47c09e14704a1e938b9ecb9dc4d
juanarrieta1986 <juanca86@gmail.com>
2021-03-31 02:56:17 +0000

    v2

--- src/models.py
+++ src/models.py
@@ -22,6 +22,7 @@ class Follower(Base):
     __tablename__ = 'follower'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
     user_from_id = Column(String(250), ForeignKey('user.id'))
     user_to_id = Column(String(250), ForeignKey('user.id'))
 
@@ -32,14 +33,14 @@ class Media(Base):
     id = Column(Integer, primary_key=True)
     mediaType = Column(String(250), nullable=False)
     url = Column(String(250), nullable=False)
-    postId = Column(String(250), , ForeignKey('post.id'))
+    postId = Column(String(250), ForeignKey('post.id'))
 
 class Post(Base):
     __tablename__ = 'post'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    userId = Column(String(250), , ForeignKey('user.id'))
+    userId = Column(String(250), ForeignKey('user.id'))
 
 class Comment(Base):
     __tablename__ = 'comment'
@@ -47,8 +48,8 @@ class Comment(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     commentText = Column(String(250), nullable=False)
-    authorId = Column(String(250), , ForeignKey('user.id'))
-    post_id = Column(String(250), , ForeignKey('post.id'))
+    authorId = Column(String(250), ForeignKey('user.id'))
+    post_id = Column(String(250), ForeignKey('post.id'))
 
     def to_dict(self):
         return {}
